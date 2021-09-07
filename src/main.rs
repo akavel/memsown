b2dad5af533f3848a0ad34ea3d966281394fe58e
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-07 09:22:11 +0200

    feat: walk dir tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use walkdir::WalkDir;
+
 fn main() {
     println!("Hello, world!");
 
@@ -9,12 +11,17 @@ fn main() {
 
     // FIXME: Stage 1: add not-yet-known files into DB
     // FIXME:  foreach *.{jpg,jpeg} in c:\fotki\...:
+    for entry in WalkDir::new(r"c:\fotki") {
+        // FIXME: if path not "*.{jpg,jpeg}" then continue
+        // TODO[LATER]: use `?` instead of .unwrap() and ret. some err from main() or print error info
+        println!("{}", entry.unwrap().path().display());
     // FIXME:    - calc sha1 hash
     // FIXME:    - extract exif date
     // FIXME:    - extract exif orientation
     // FIXME:    - create 200x200 thumbnail
     // FIXME:       - lanczos resizing
     // FIXME:       - deoriented
+    }
     // FIXME: Stage 2: scan all files once more and refresh them in DB
 }
 
