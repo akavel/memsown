90491bf0e369d95b5e8cacb4b76e8626420ddf18
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-07 09:14:24 +0200

    doc: describe initial impl. outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
+    // mdb = openDb("backer.db")
+    // let markers = @[
+    //   r"d:\backer-id.json",
+    //   r"c:\fotki\backer-id.json",
+    // ]
+
+    // FIXME: Stage 1: add not-yet-known files into DB
+    // FIXME:  foreach *.{jpg,jpeg} in c:\fotki\...:
+    // FIXME:    - calc sha1 hash
+    // FIXME:    - extract exif date
+    // FIXME:    - extract exif orientation
+    // FIXME:    - create 200x200 thumbnail
+    // FIXME:       - lanczos resizing
+    // FIXME:       - deoriented
+    // FIXME: Stage 2: scan all files once more and refresh them in DB
 }
+
