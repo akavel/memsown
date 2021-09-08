8b0813d623131a14bcce103056cb20ce149afc62
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-08 10:01:07 +0200

    feat: only print jpeg filenames

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use walkdir::WalkDir;
+use std::ffi::OsStr;
 
 fn main() {
     println!("Hello, world!");
@@ -15,6 +16,26 @@ fn main() {
         // TODO[LATER]: use `?` instead of .unwrap() and ret. some err from main() or print error info
         let f = entry.unwrap();
         if !f.file_type().is_file() { continue; }
+        // TODO[LATER]: (how) can we make below block shorter & prettier?
+        // let ext = f.path().extension().map(|s| s.to_ascii_lowercase().to_str()).flatten();
+        let ext = f.path().extension().map(OsStr::to_str).flatten().map(|s| s.to_ascii_lowercase());
+        // println!("{:?}", ext);
+        match ext.as_deref() {
+            Some("jpg") | Some("jpeg") => {},
+            _ => continue
+        }
+        // let ext = f.path().extension().map(OsStr::to_ascii_lowercase);
+        // if ext.is_none() { continue; }
+
+        // if let Some(s) = ext && (s == ".jpg" || s == ".jpeg") {
+        //     // ok
+        // } else {
+        //     continue;
+        // }
+        // match f.path().extension().map(|s| s.to_ascii_lowercase()) {
+        //     Some(".jpg") | Some(".jpeg") => {},
+        //     _ => continue
+        // }
         // FIXME: if path not "*.{jpg,jpeg}" then continue
         println!("{}", f.path().display());
     // FIXME:    - calc sha1 hash
