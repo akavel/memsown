5d21977b9ada20b85bfbc1c14feb0a3da5eddda6
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-09 09:50:48 +0200

    feat: parse Exif block from jpegs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use walkdir::WalkDir;
 use std::ffi::OsStr;
+use std::fs::read;
+use std::io::Cursor;
+
+use exif::{Reader};
+use walkdir::WalkDir;
 
 fn main() {
     println!("Hello, world!");
@@ -16,21 +20,27 @@ fn main() {
         // TODO[LATER]: use `?` instead of .unwrap() and ret. some err from main() or print error info
         let f = entry.unwrap();
 
-        // We're interested only in files, with .jpg/.jpeg extension
+        // We're interested only in files, and only with .jpg/.jpeg extension
         if !f.file_type().is_file() { continue; }
         let ext = f.path().extension().map(OsStr::to_str).flatten().map(str::to_ascii_lowercase);
         match ext.as_deref() {
             Some("jpg") | Some("jpeg") => (),
             _ => continue
         }
+        let buf = read(f.path()).unwrap();
 
-        println!("{}", f.path().display());
     // FIXME:    - calc sha1 hash
-    // FIXME:    - extract exif date
-    // FIXME:    - extract exif orientation
+
+        // FIXME:    - extract exif date
+        // FIXME:    - extract exif orientation
+        let exif = Reader::new().read_from_container(&mut Cursor::new(buf)).unwrap();
+        // let exif = Reader::new().read_from_container(buf).unwrap();
+
     // FIXME:    - create 200x200 thumbnail
     // FIXME:       - lanczos resizing
     // FIXME:       - deoriented
+
+        println!("{}", f.path().display());
     }
     // FIXME: Stage 2: scan all files once more and refresh them in DB
 }
