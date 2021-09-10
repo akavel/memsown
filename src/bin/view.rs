9582349f42eddb31e552f54cd2b86ede831f484b
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-10 16:14:39 +0200

    feat(view): hello-world window using iced crate

--- src/bin/view.rs
+++ src/bin/view.rs
@@ -1,3 +1,42 @@
-fn main() {
+use anyhow::Result;
+use iced::Sandbox;
+use rusqlite::{params, Connection as DbConnection};
+
+fn main() -> iced::Result {
     println!("Hello view");
+
+
+    // FIXME: Milestone: just show single thumbnail from DB, using iced crate
+
+    // FIXME: scrollable window with Gallery widget inside
+    // FIXME: Gallery widget showing thumbnails & date separators, based on DB contents
+
+    Gallery::run(iced::Settings::default())
+}
+
+struct Gallery {
+    db: DbConnection,
+}
+
+impl iced::Sandbox for Gallery {
+    type Message = ();
+
+    fn new() -> Gallery {
+        Gallery{
+            db: DbConnection::open("backer.db").unwrap(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Backer") // TODO[LATER]: description and/or status info and/or version
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // FIXME
+    }
+
+    fn view(&mut self) -> iced::Element<Self::Message> {
+        use iced::Text;
+        Text::new("Hello, world!").into()
+    }
 }
