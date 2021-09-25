f5f8457a68dbca6a6895f07901af86e3533b8cca
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-25 20:47:31 +0200

    feat(preview): start work on big image preview

---
+++ src/bin/preview.rs
@@ -0,0 +1,32 @@
+use iced::Sandbox;
+
+fn main() -> iced::Result {
+    println!("Hello preview");
+
+    Preview::run(iced::Settings::default())
+}
+
+struct Preview {
+}
+
+impl iced::Sandbox for Preview {
+    type Message = ();
+
+    fn new() -> Preview {
+        Preview { }
+    }
+
+    fn title(&self) -> String {
+        String::from("Backer image preview") // FIXME[LATER]
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // FIXME
+    }
+
+    fn view(&mut self) -> iced::Element<Self::Message> {
+        let path = r"C:\fotki\incoming - z telefonu\20170426_124522.jpg";
+
+        iced::widget::image::Image::new(path).into()
+    }
+}
