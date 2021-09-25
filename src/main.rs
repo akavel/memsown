061674d40460ea021450e55005b88d27d54b9134
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-25 19:26:20 +0200

    feat(backer): read from multiple trees, sequentially

--- src/main.rs
+++ src/main.rs
@@ -33,76 +33,92 @@ fn main() -> Result<()> {
     ];
 
     // FIXME: Milestone: read from multiple marker roots in parallel
-    let (root, marker) = marker_read(marker_paths[1])?;
-    println!("marker {} at: {}", &marker, root.display());
+    for (i, marker) in marker_paths.iter().enumerate() {
+        // TODO[LATER]: extract loop contents into a fn for readability
 
-    // Stage 1: add not-yet-known files into DB
-    // TODO[LATER]: in parallel thread, count all matching files, then when done start showing progress bar/percentage
-    let images = GlobWalkerBuilder::new(&root, "*.{jpg,jpeg}")
-        .case_insensitive(true)
-        .file_type(globwalk::FileType::FILE)
-        .build();
-    for entry in images? {
-        let path = entry?.path().to_owned();
-        let buf = read(&path)?;
-
-        let os_relative = path.strip_prefix(&root)?;
-        let relative = os_relative
-            .to_slash()
-            .with_context(|| format!("Failed to convert path {:?} to slash-based", os_relative))?;
-        let db_read = db.read().unwrap();
-        if db_exists(&db_read, &marker, &relative)? {
-            stdout.write_all(b".")?;
-            stdout.flush()?;
-            continue;
-        }
-        drop(db_read);
-
-        // Calculate sha1 hash of the file contents.
-        // TODO[LATER]: maybe switch to a secure hash (sha2 or other, see: https://github.com/RustCrypto/hashes)
-        let hash = format!("{:x}", Sha1::digest(&buf));
-
-        // Does the JPEG have Exif block? We assume it'd be the most reliable source of metadata.
-        let exif = ExifReader::new()
-            .read_from_container(&mut Cursor::new(&buf))
-            .ok();
-        let date = try_deduce_date(exif.as_ref(), &relative);
-        // // TODO[LATER]: use some orientation enum / stricter type instead of raw u16
-        // // TODO[LATER]: test exif deorienting with cases from: https://github.com/recurser/exif-orientation-examples
-        // // (see also: https://www.daveperrett.com/articles/2012/07/28/exif-orientation-handling-is-a-ghetto)
-        // let orientation = exif.as_ref().and_then(exif_orientation).unwrap_or(1);
-
-        let img = match ImageReader::new(Cursor::new(&buf))
-            .with_guessed_format()?
-            .decode()
-        {
-            Ok(img) => img,
+        let (root, marker) = match marker_read(marker) {
+            Ok(m) => Ok(m),
             Err(err) => {
-                // TODO[LATER]: use termcolor crate to print errors in red
-                // FIXME[LATER]: resolve JPEG decoding error: "spectral selection is not allowed in non-progressive scan"
-                eprintln!("\nFailed to decode JPEG {:?}, skipping: {}", &path, err);
+                if let Some(cause) = err.downcast_ref::<std::io::Error>() {
+                    if cause.kind() == std::io::ErrorKind::NotFound {
+                        println!("\nSkipping tree at '{}': {}", marker, error_chain(&err));
+                        continue;
+                    }
+                }
+                Err(err)
+            },
+        }?;
+        println!("marker {} at: {}", &marker, root.display());
+
+        // Stage 1: add not-yet-known files into DB
+        // TODO[LATER]: in parallel thread, count all matching files, then when done start showing progress bar/percentage
+        let images = GlobWalkerBuilder::new(&root, "*.{jpg,jpeg}")
+            .case_insensitive(true)
+            .file_type(globwalk::FileType::FILE)
+            .build();
+        for entry in images? {
+            let path = entry?.path().to_owned();
+            let buf = read(&path)?;
+
+            let os_relative = path.strip_prefix(&root)?;
+            let relative = os_relative
+                .to_slash()
+                .with_context(|| format!("Failed to convert path {:?} to slash-based", os_relative))?;
+            let db_read = db.read().unwrap();
+            if db_exists(&db_read, &marker, &relative)? {
+                stdout.write_all(b".")?;
+                stdout.flush()?;
                 continue;
             }
-        };
-        // let thumb = img.resize(200, 200, FilterType::Lanczos3);
-        let thumb = img.resize(200, 200, FilterType::CatmullRom);
-        // FIXME[LATER]: fix the thumbnail's orientation
-        let mut thumb_jpeg = Vec::<u8>::new();
-        thumb.write_to(&mut thumb_jpeg, image::ImageOutputFormat::Jpeg(90))?;
+            drop(db_read);
 
-        let info = backer::model::FileInfo {
-            hash: hash.clone(),
-            date,
-            thumb: thumb_jpeg,
-        };
-        let db_write = db.write().unwrap();
-        db_upsert(&db_write, &marker, &relative, &info)?;
-        drop(db_write);
+            // Calculate sha1 hash of the file contents.
+            // TODO[LATER]: maybe switch to a secure hash (sha2 or other, see: https://github.com/RustCrypto/hashes)
+            let hash = format!("{:x}", Sha1::digest(&buf));
 
-        stdout.write_all(b"+")?;
-        stdout.flush()?;
-        // println!("{} {} {:?} {:?}", &hash, path.display(), date.map(|d| d.to_string()), orientation);
+            // Does the JPEG have Exif block? We assume it'd be the most reliable source of metadata.
+            let exif = ExifReader::new()
+                .read_from_container(&mut Cursor::new(&buf))
+                .ok();
+            let date = try_deduce_date(exif.as_ref(), &relative);
+            // // TODO[LATER]: use some orientation enum / stricter type instead of raw u16
+            // // TODO[LATER]: test exif deorienting with cases from: https://github.com/recurser/exif-orientation-examples
+            // // (see also: https://www.daveperrett.com/articles/2012/07/28/exif-orientation-handling-is-a-ghetto)
+            // let orientation = exif.as_ref().and_then(exif_orientation).unwrap_or(1);
+
+            let img = match ImageReader::new(Cursor::new(&buf))
+                .with_guessed_format()?
+                .decode()
+            {
+                Ok(img) => img,
+                Err(err) => {
+                    // TODO[LATER]: use termcolor crate to print errors in red
+                    // FIXME[LATER]: resolve JPEG decoding error: "spectral selection is not allowed in non-progressive scan"
+                    eprintln!("\nFailed to decode JPEG {:?}, skipping: {}", &path, err);
+                    continue;
+                }
+            };
+            // let thumb = img.resize(200, 200, FilterType::Lanczos3);
+            let thumb = img.resize(200, 200, FilterType::CatmullRom);
+            // FIXME[LATER]: fix the thumbnail's orientation
+            let mut thumb_jpeg = Vec::<u8>::new();
+            thumb.write_to(&mut thumb_jpeg, image::ImageOutputFormat::Jpeg(90))?;
+
+            let info = backer::model::FileInfo {
+                hash: hash.clone(),
+                date,
+                thumb: thumb_jpeg,
+            };
+            let db_write = db.write().unwrap();
+            db_upsert(&db_write, &marker, &relative, &info)?;
+            drop(db_write);
+
+            print!("{}", i);
+            stdout.flush()?;
+            // println!("{} {} {:?} {:?}", &hash, path.display(), date.map(|d| d.to_string()), orientation);
+        }
     }
+
 
     // FIXME: Stage 2: check if all files from DB are present on disk, delete entries for any missing
 
@@ -116,7 +132,7 @@ fn marker_read(file_path: &str) -> Result<(PathBuf, String)> {
     let file_path = Path::new(file_path);
     let parent = file_path.parent().ok_or_else(|| {
         anyhow!(
-            "could not split parent directory of: {}",
+            "Could not split parent directory of '{}'",
             file_path.display()
         )
     })?;
@@ -126,10 +142,15 @@ fn marker_read(file_path: &str) -> Result<(PathBuf, String)> {
     struct Marker {
         id: String,
     }
-    let file = File::open(file_path)?;
+    let file = File::open(file_path)
+        .with_context(|| format!("Failed to open '{}'", file_path.display()))?;
     let m: Marker = serde_json::from_reader(std::io::BufReader::new(file))?;
 
     Ok((parent.to_owned(), m.id))
+}
+
+fn error_chain(err: &anyhow::Error) -> String {
+    err.chain().into_iter().map(|e| e.to_string()).collect::<Vec<String>>().join(": ")
 }
 
 fn db_init(db: &DbConnection) -> ::rusqlite::Result<()> {
