769377d56468cd8392e207f8494352c25792e1b5
Mateusz Czapliński <czapkofan@gmail.com>
2021-09-29 18:46:32 +0200

    doc(preview): plan for contributing deorientation to iced

--- src/bin/preview.rs
+++ src/bin/preview.rs
@@ -27,6 +27,19 @@ impl iced::Sandbox for Preview {
         let path = r"C:\fotki\incoming - z telefonu\20170426_124522.jpg";
 
         // FIXME: Milestone: deorient the image [apply matrix possibly]
+        // see: https://github.com/iced-rs/iced/discussions/1064
+        // - write helper GUI app previewing effects on the testsuite of images from links below
+        // - add local checkouted dependency of iced into the sample's Cargo.toml
+        // - add Exif dependency in iced
+        // - parse Exif data from memory or disk (if possible)
+        // - extract Exif orientation info
+        // - translate orientation info to proper transforms
+        // - perform the transforms on the in-memory buffer
+        //
+        // SEE ALSO:
+        // - https://magnushoff.com/articles/jpeg-orientation/
+        // - https://github.com/recurser/exif-orientation-examples
+        // - https://www.daveperrett.com/articles/2012/07/28/exif-orientation-handling-is-a-ghetto
 
         iced::widget::image::Image::new(path).into()
     }
