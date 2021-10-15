0fecc10507fa917dadc337d55a299b7e0ecc6b1c
Mateusz Czapliński <czapkofan@gmail.com>
2021-10-15 10:46:52 +0200

    doc: add TODOs for next features

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,22 @@ use sha1::{Digest, Sha1};
 
 use backer::imaging::*;
 
+// TODO[LATER]: load marker_paths from JSON
+// TODO: load date-from-path regexps from JSON:
+//   {"paths-to-dates": {"sf7-c-fotki": {".*(20\d\d)-(\d\d)-(\d\d)": "\1-\2-\3"}}}
+// TODO: introduce "dry-run" mode for testing date-from-path regexps
+// TODO: use date-from-path regexps
+// TODO: skip small images (threshold size configurable in JSON - dimensions or bytes?)
+// - or maybe just skip them in viewer for now?
+// TODO: re-scan disks and re-generate data in DB
+// - for above step, we should probably have an iterator easily generating file paths from marker
+//   path (similar as already exists for initial scan, but extracted for reuse)
+// TODO: delete files not found on disk from DB (only locations - keep "files")
+// - for above step, we need to be able to easily check if specific path exists at location in
+//   specific marker's tree
+//   - it should use same filters as the main files iterator (incl. extension, jpeg size)
+// TODO: merge 'view' and 'main' binaries
+// TODO: deorient thumbnails stored into DB
 
 fn main() -> Result<()> {
     // TODO[LATER]: run rustfmt on this repo
