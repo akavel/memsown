df1ae52c1c810868d2400dc541ec39abc8494acf
Mateusz Czapliński <czapkofan@gmail.com>
2022-01-19 12:25:30 +0100

    feat(pathwalk): walker::Files::iter()

--- src/pathwalk.rs
+++ src/pathwalk.rs
@@ -71,7 +71,24 @@ pub mod matcher {
 pub mod walker {
     use std::path::{Path, PathBuf};
 
+    use anyhow::{anyhow, Result};
+    use walkdir::WalkDir;
+
     use super::matcher as m;
+
+    /// Note: for now implicitly assumed to be a file, not a dir.
+    /// (Behavior for symlinks is currently unspecified.)
+    // TODO[LATER]: try making this thin wrapper around `&Path`
+    #[derive(Debug)]
+    pub struct DirEntry {
+        relative_path: PathBuf,
+    }
+
+    impl DirEntry {
+        fn relative_path(&self) -> &Path {
+            self.relative_path.as_ref()
+        }
+    }
 
     pub struct Files {
         root: PathBuf,
@@ -87,6 +104,58 @@ pub mod walker {
             Self {
                 root: root.as_ref().into(),
                 matchers: Vec::from_iter(matchers),
+            }
+        }
+
+        // TODO[LATER]: also impl IntoIterator (move semantics, for loops)
+        // TODO[LATER]: try moving Result to `new` func instead
+        // TODO[LATER]: custom Result type instead of anyhow
+        pub fn iter(&self) -> FilesIterator {
+            FilesIterator {
+                root: self.root.clone(),
+                iter: WalkDir::new(&self.root).into_iter(),
+            }
+        }
+    }
+
+    // impl IntoIterator for Files {
+    //     type Item = DirEntry;
+    //     type IntoIter = FilesIterator;
+    //     fn into_iter(self)
+    // }
+
+    pub struct FilesIterator {
+        // TODO[LATER]: make `root` a `&Path`
+        root: PathBuf,
+        iter: walkdir::IntoIter,
+    }
+
+    impl Iterator for FilesIterator {
+        type Item = Result<DirEntry>;
+        fn next(&mut self) -> Option<Self::Item> {
+            loop {
+                // Basic iterator pass-through of errors & iteration end.
+                let entry = match self.iter.next() {
+                    None => return None,
+                    Some(Err(err)) => return Some(Err(anyhow!(err))),
+                    Some(Ok(entry)) => entry,
+                };
+                // Don't emit directory entries; emit error for symlinks.
+                let kind = entry.file_type();
+                if kind.is_dir() {
+                    continue;
+                } else if kind.is_symlink() {
+                    return Some(Err(anyhow!("Don't know what to do with a symbolic link: {:?}", entry.path())));
+                }
+                // Extract relative path.
+                // Note: walkdir pinky-promises that paths will have the prefix, so we should be
+                // safe to just `unwrap()`; but we already have Result return type, so we can just
+                // squeeze another error there instead, just in case.
+                let relative_path = match entry.path().strip_prefix(&self.root) {
+                    Err(err) => return Some(Err(anyhow!("Failed to split relative path: {}", err))),
+                    Ok(path) => path,
+                };
+                return Some(Ok(DirEntry{ relative_path: relative_path.into() }))
             }
         }
     }
