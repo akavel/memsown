f5d86eb5ff2cdef0b135669f79ea5688ff4efe2f
Mateusz Czapliński <czapkofan@gmail.com>
2023-12-02 11:32:20 +0100

    build: import tracing-chrome and deps

--- src/bin/view.rs
+++ src/bin/view.rs
@@ -1,10 +1,15 @@
 use iced::pure::Application;
+use tracing_chrome::ChromeLayerBuilder as ChromiumTracingBuilder;
+use tracing_subscriber::{registry::Registry, prelude::*};
 
 use backer::db;
 use backer::gui::Gui;
 
 fn main() -> iced::Result {
     println!("Hello view");
+
+    let (chromium_tracing, _guard) = ChromiumTracingBuilder::new().build();
+    tracing_subscriber::registry().with(chromium_tracing).init();
 
     let db = db::open("backer.db").unwrap();
 
