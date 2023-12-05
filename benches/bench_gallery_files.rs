e186f2e3fc575c77271f09f78d474480e6725b36
Mateusz Czapliński <czapkofan@gmail.com>
2023-12-05 08:57:05 +0100

    bench: WIP adding files to sample data

--- benches/bench_gallery_files.rs
+++ benches/bench_gallery_files.rs
@@ -1,3 +1,4 @@
+use chrono::naive::{NaiveDate, NaiveDateTime};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn bench_gallery_files(c: &mut Criterion) {
@@ -10,13 +11,30 @@ criterion_main!(benches);
 fn setup_db_with_tags() -> db::Connection {
     // FIXME: for benchmarks, should we use on-disk database for consistency with real-life results?
     let conn = db::open_in_memory();
-    // FIXME: insert some sample data
+
+    // insert some sample data
+    // TODO: write helper macro to reduce repetition & improve readability
+
+    // sample tags
     let sql = "INSERT INTO tag(name, hidden) VALUES(?,?)";
     conn.execute(sql, params!["foo", true]).unwrap();
+    let id_tag_foo = conn.last_insert_rowid();
     conn.execute(sql, params!["bar", false]).unwrap();
+    let id_tag_bar = conn.last_insert_rowid();
 
-    // FIXME: insert sample files
-    // let sql = 
+    // sample files
+    let sql = "INSERT INTO file(hash, date) VALUES(?,?)";
+    let date: Option<NaiveDateTime> = None;
+    conn.execute(sql, params!["hash1", date]).unwrap();
+    let id_file1 = conn.last_insert_rowid();
+    let date: Option<NaiveDateTime> = Some(NaiveDate::from_ymd(
+            2023,12,05).and_hms(8,53,12));
+    conn.execute(sql, params!["hash2", date]).unwrap();
+    let id_file2 = conn.last_insert_rowid();
+    let date: Option<NaiveDateTime> = Some(NaiveDate::from_ymd(
+            2022,01,02).and_hms(16,00,01));
+    conn.execute(sql, params!["hash3", date]).unwrap();
+    let id_file3 = conn.last_insert_rowid();
 
     // FIXME: connect tags with files - table file_tag
 
