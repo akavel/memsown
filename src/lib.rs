dc0db76fd085f91edd936be4b89da58b26c4bde0
Mateusz Czapliński <czapkofan@gmail.com>
2024-01-06 10:42:30 +0100

    (cargo fmt)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 //! Module containing a generic iterator returning typed results from cached SQLite queries.
 
-use rusqlite::{Params, Connection, Row, Result};
+use rusqlite::{Connection, Params, Result, Row};
 
 struct Typed<F> {
     f: F,
 }
 
-impl<T, F> Typed<F> 
-where 
-    F: FnMut(&Row) -> Result<T>
+impl<T, F> Typed<F>
+where
     // F: FnMut(&Row<'_>) -> Result<T>
     // F: FnMut(&Row<'stmt>) -> Result<T>
+    F: FnMut(&Row) -> Result<T>,
 {
-    fn new<P>(conn: &Connection, sql: &str, params: P, f: F) -> Self 
-    where 
+    fn new<P>(conn: &Connection, sql: &str, params: P, f: F) -> Self
+    where
         P: Params,
     {
         Self { f }
@@ -21,7 +21,9 @@ where
 }
 
 impl<T, F> Iterator for Typed<F>
-where F: FnMut(&Row) -> Result<T> {
+where
+    F: FnMut(&Row) -> Result<T>,
+{
     type Item = Result<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -29,7 +31,6 @@ where F: FnMut(&Row) -> Result<T> {
         None
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -42,10 +43,10 @@ mod test {
         let iter = simple_iter(&conn);
         let maybe_all = iter.collect::<Result<Vec<_>>>();
         let all = maybe_all.unwrap();
-        assert_eq!(all, &[
-            ("hello".to_string(), 1i64),
-            ("world".to_string(), 2i64),
-        ]);
+        assert_eq!(
+            all,
+            &[("hello".to_string(), 1i64), ("world".to_string(), 2i64),]
+        );
     }
 
     fn simple_iter(conn: &Connection) -> impl Iterator<Item = Result<(String, i64)>> {
@@ -69,9 +70,9 @@ mod test {
         db.execute_batch(
             "CREATE TABLE IF NOT EXISTS foobar(foo TEXT, bar INTEGER);
             INSERT INTO foobar(foo, bar) VALUES('hello', 1);
-            INSERT INTO foobar(foo, bar) VALUES('world', 2);"
-        ).unwrap();
+            INSERT INTO foobar(foo, bar) VALUES('world', 2);",
+        )
+        .unwrap();
         db
     }
 }
-
